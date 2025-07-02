6163f276e59f66b683ffbf3e641372f26a420d33
palrang22 <kseunghui@gmail.com>
2025-07-02 22:42:19 +0900

    initial commit

---
+++ Talet/Talet/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Talet
+//
+//  Created by 김승희 on 7/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
