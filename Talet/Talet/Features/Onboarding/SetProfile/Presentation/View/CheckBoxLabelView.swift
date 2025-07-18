97df784ad39fe1c04ece791c93bdcb7dff928d8b
palrang22 <kseunghui@gmail.com>
2025-07-18 19:31:20 +0900

    [Feat] 체크박스+label+버튼 커스텀뷰

--- Talet/Talet/Features/Onboarding/SetProfile/Presentation/View/CheckBoxLabelView.swift
+++ Talet/Talet/Features/Onboarding/SetProfile/Presentation/View/CheckBoxLabelView.swift
@@ -16,12 +16,68 @@ class CheckBoxLabelView: UIView {
     //MARK: Properties
     
     //MARK: UI Components
+    let checkBoxButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "square"), for: .normal)
+        button.setImage(UIImage(systemName: "checkmark.square.fill"), for: .selected)
+        button.tintColor = .systemBlue
+        return button
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .label
+        return label
+    }()
+    
+    // 버튼 처리방법 확인후 추후연결
+    let labelStyleButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("내용보기", for: .normal)
+        button.isHidden = true
+        return button
+    }()
     
     //MARK: init
+    init(text: String, linkURL: URL? = nil) {
+        super.init(frame: .zero)
+        titleLabel.text = text
+        if let url = linkURL {
+            labelStyleButton.isHidden = false
+        } else {
+            labelStyleButton.isHidden = true
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     //MARK: Methods
     
     //MARK: Bindings
     
     //MARK: Layout
+    private func setLayout() {
+        [checkBoxButton,
+         titleLabel,
+         labelStyleButton].forEach { addSubview($0) }
+        
+        checkBoxButton.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(24)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(checkBoxButton.snp.trailing).offset(12)
+            $0.centerY.equalToSuperview()
+        }
+        
+        labelStyleButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+    }
 }
