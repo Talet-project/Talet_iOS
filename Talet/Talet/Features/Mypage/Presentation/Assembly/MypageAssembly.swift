2f1893f3b4b842e6177113fe31b78823acb780bd
merseong <eogud8203@naver.com>
2025-07-31 01:08:19 +0900

    [Feat] 탭바 이미지 추가 및 디테일 설정 가능하도록 구현, Color HEX값으로 입력 가능하도록 커스텀

---
+++ Talet/Talet/Features/Mypage/Presentation/Assembly/MypageAssembly.swift
@@ -0,0 +1,8 @@
+//
+//  MypageAssembly.swift
+//  Talet
+//
+//  Created by 윤대성 on 7/30/25.
+//
+
+import Foundation
---
+++ Talet/Talet/Resources/Extension/UIColor+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Extension.swift
+//  Talet
+//
+//  Created by 윤대성 on 7/31/25.
+//
+
+import Foundation
