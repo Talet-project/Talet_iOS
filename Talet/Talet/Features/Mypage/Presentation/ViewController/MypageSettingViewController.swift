0edbe73bc3ca0b0a1f6a91233c5084a664985863
palrang22 <kseunghui@gmail.com>
2025-12-30 01:25:41 +0900

    [feat] mypagesettingvc 작업

--- Talet/Talet/Features/Mypage/Presentation/ViewController/MypageSettingViewController.swift
+++ Talet/Talet/Features/Mypage/Presentation/ViewController/MypageSettingViewController.swift
@@ -17,6 +17,15 @@ class MypageSettingViewController: UIViewController {
     //MARK: Properties
     
     //MARK: UI Components
+    private let tableView = UITableView(frame: .zero, style: .grouped).then {
+        $0.backgroundColor = .white
+        $0.separatorStyle = .singleLine
+        $0.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        $0.separatorColor = .gray200
+        $0.sectionHeaderHeight = 0
+        $0.sectionFooterHeight = 0
+        $0.isScrollEnabled = false
+    }
     
     //MARK: init
     init() {
@@ -30,21 +39,98 @@ class MypageSettingViewController: UIViewController {
     //MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind()
+        setNavigationBar()
         setLayout()
+        setupTableView()
     }
     
     //MARK: Methods
-    
-    //MARK: Bindings
-    private func bind() {
+    private func setNavigationBar() {
+        self.title = "설정"
         
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+        appearance.shadowColor = .clear
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        let backButton = UIButton().then {
+            $0.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+            $0.tintColor = .black
+            $0.addAction(UIAction(handler: { [weak self] _ in
+                self?.navigationController?.popViewController(animated: true)
+            }), for: .touchUpInside)
+        }
+        let backItem = UIBarButtonItem(customView: backButton)
+        self.navigationItem.leftBarButtonItem = backItem
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(MypageSettingCell.self, forCellReuseIdentifier: MypageSettingCell.id)
     }
     
     //MARK: Layout
     private func setLayout() {
-        view.backgroundColor = .white
+        view.backgroundColor = .gray50
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(180)
+        }
+    }
+}
+
+//MARK: Extensions
+extension MypageSettingViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-    //MARK: Extensions
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView
+            .dequeueReusableCell(withIdentifier: MypageSettingCell.id,
+                                 for: indexPath) as? MypageSettingCell else {
+            return UITableViewCell()
+        }
+        
+        let titles = ["서비스 이용약관", "로그아웃", "탈퇴하기"]
+        cell.configure(title: titles[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return .leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            print("서비스 이용약관 선택")
+        case 1:
+            print("로그아웃 선택")
+        case 2:
+            print("탈퇴하기 선택")
+        default:
+            break
+        }
+    }
 }
